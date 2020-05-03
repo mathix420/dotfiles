1db5b9c1dcf9aa0505d03b0d5a577bfc994b0a8b
Arnaud Gissinger <agissing@student.42.fr>
2020-05-03 13:31:47 +0200

    [AUTO-COMMIT] 2020-05-03 13:31:47.563072

---
+++ i3/scripts/screen_manager.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+match=$(xrandr | grep -oE "^(DP-[1-2]) connected" | cut -d' ' -f1)
+
+if test -n "$match"; then
+    xrandr --output $match --auto --right-of eDP-1
+    echo "$match set right of eDP-1";
+fi
