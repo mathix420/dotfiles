e9bca6bd7a13b9c37b1a84afbe2ff05233c1c4cf
Arnaud Gissinger <agissing@student.42.fr>
2020-05-16 19:19:30 +0200

    [AUTO-COMMIT] 2020-05-16 19:19:30.188071

--- i3/scripts/screen_manager.sh
+++ i3/scripts/screen_manager.sh
@@ -5,4 +5,7 @@ match=$(xrandr | grep -oE "^(DP-[1-2]) connected" | cut -d' ' -f1)
 if test -n "$match"; then
     xrandr --output $match --auto --right-of eDP-1
     echo "$match set right of eDP-1";
+else
+    xrandr --auto
+    echo "reset current setup";
 fi
