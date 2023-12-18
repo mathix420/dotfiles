83dbb825d518b8e55d95db640c5f78375a822609
Arnaud Gissinger <agissing@student.42.fr>
2023-12-18 12:37:09 +0100

    [AUTO-COMMIT] 2023-12-18 12:37:09.761000

---
+++ .emacs.d/elpa/timu-caribbean-theme-1.5/timu-caribbean-theme-pkg.el
@@ -0,0 +1,14 @@
+(define-package "timu-caribbean-theme" "1.5" "Color theme with cyan as a dominant color"
+  '((emacs "27.1"))
+  :commit "af60151fe35bd1c780b7c4a37032699989ee6162" :authors
+  '(("Aimé Bertrand" . "aime.bertrand@macowners.club"))
+  :maintainers
+  '(("Aimé Bertrand" . "aime.bertrand@macowners.club"))
+  :maintainer
+  '("Aimé Bertrand" . "aime.bertrand@macowners.club")
+  :keywords
+  '("faces" "themes")
+  :url "https://gitlab.com/aimebertrand/timu-caribbean-theme")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,12 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
@@ -15,10 +21,10 @@
 (column-number-mode 1)
 
 ;; 42 scripts
-(load "list.el")
-(load "string.el")
-(load "comments.el")
-(load "header.el")
+;; (load "list.el")
+;; (load "string.el")
+;; (load "comments.el")
+;; (load "header.el")
 
 ;; Dart mode
 (add-to-list 'load-path "~/.emacs.d/lisp/dart-mode/")
@@ -27,26 +33,21 @@
 (add-to-list 'auto-mode-alist '("\\.dart\\'" . dart-mode))
 (autoload 'dart-mode "dart-mode")
 
-;; Forcing background color
-(add-to-list 'default-frame-alist '(background-color . "#1c1c1c"))
+;; Theme
+(load-theme 'timu-caribbean t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#1c1c1c" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes '(tsdh-dark))
- '(gud-gdb-command-name "gdb --annotate=1")
- '(inhibit-startup-screen t)
- '(large-file-warning-threshold nil)
+
  '(package-archives
    '(("gnu" . "https://elpa.gnu.org/packages/")
 	 ("melpa" . "https://stable.melpa.org/packages/")))
- '(package-selected-packages '(typescript-mode lsp-mode rust-mode vue-mode yaml-mode)))
+ '(package-selected-packages
+   '(timu-caribbean-theme typescript-mode lsp-mode rust-mode vue-mode yaml-mode)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -54,20 +55,28 @@
  ;; If there is more than one, they won't work right.
  )
 
+(custom-theme-set-faces
+ 'timu-caribbean
+
+ `(default ((t (:background , nil))))
+ `(region ((t (:background "#0d0d0d"))))
+ `(mode-line ((t (:background nil))))
+ `(mode-line-inactive ((t (:background nil))))
+ )
+
 ;; Configure indent for C
 (c-set-offset 'substatement-open 0)
 (setq-default indent-tabs-mode t)
 (setq-default tab-width 4)
+(setq c-basic-offset tab-width)
 (defvaralias 'c-basic-offset 'tab-width)
 
-;; Delete full tabs on one backspace
+;; full tabs on one backspace
 (setq-default c-backspace-function 'delete-backward-char)
 
 ;; Highlight trailing whitespace.
 (setq-default show-trailing-whitespace t)
 (set-face-background 'trailing-whitespace "color-129")
 
-;; Background alpha
-(set-frame-parameter nil 'alpha-background 70)
-
-(add-to-list 'default-frame-alist '(alpha-background . 70))
+;; Disable menu bar
+(menu-bar-mode -1)
